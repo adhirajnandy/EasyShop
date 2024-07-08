3fdce0fcbb280178a7d0fb699d468a8123ffccec
adhirajnandy <adhirajnandy9@gmail.com>
2024-07-08 21:41:35 +0530

    [UPD]

--- createUploadsDir.js
+++ createUploadsDir.js
@@ -1,5 +1,10 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const uploadDir = path.join(__dirname, "uploads");
 
