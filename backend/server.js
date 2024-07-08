ca4ddf221190e055f5c6e7a570a2645a2647ecf5
adhirajnandy <adhirajnandy9@gmail.com>
2024-07-08 19:51:58 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
-import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-import uploadCsvRoutes from './routes/uploadCsvRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import uploadCsvRoutes from "./routes/uploadCsvRoutes.js";
 
 //for making connection to MongoDB
 connectDB();
@@ -21,29 +20,39 @@ const app = express();
 
 //Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-//These two lines of code allows us to get the body data that we send while user authorization 
+app.use(express.urlencoded({ extended: true }));
+//These two lines of code allows us to get the body data that we send while user authorization
 //that is email and password that we send while authenticating the user
 
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get('/',(req,res) => {
-    res.send('api is running');
-});
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/uploadCSV", uploadCsvRoutes);
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/upload', uploadRoutes);
-app.use('/api/uploadCSV', uploadCsvRoutes);
-
-app.get('/api/config/paypal', (req, res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}) );
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
 const __dirname = path.resolve(); //Setting __dirname to current directory
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("api is running");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
-
-app.listen(port, () => console.log(`Server running on ${port}`))
+app.listen(port, () => console.log(`Server running on ${port}`));
